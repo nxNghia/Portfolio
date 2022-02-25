0b17400eb09c37af5fcbb1a67f4cbb90ef77fabb
nxNghia <nghia1421999@gmail.com>
2022-02-25 23:12:34 +0700

    remove redundant package

--- src/Components/MainView/Home/Contact/contactItem.js
+++
@@ -1,9 +0,0 @@
-import { Avatar } from "@material-ui/core"
-
-const ContactItem = ({item, _style_ = null}) => {
-    return (
-        <a href='https://google.com'><Avatar src={item} className={_style_} alt='item'></Avatar></a>
-    )
-}
-
-export default ContactItem
